5d7b0a47b5e82bd49a70d6b7c5779c7f50a10a32
Margot Konig <margot.konig@blackrock.com>
2019-05-28 17:32:22 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :items
+  
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+    @transactions = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    transaction = quantity * price
+      @total += transaction
+      @transactions << transaction
+      quantity.times do
+      @items << title
+ end
+  end
+  
+  def apply_discount
+    # discount comes in as 20(%) instead of .20, so we divide by 100.0 so it is float and not an integer -- integers cannot have decimal values
+    @total = @total - (@total*(discount/100.0))
+   if discount > 0 
+     "After the discount, the total comes to $#{@total.to_i}."
+   else
+     "There is no discount to apply."
+   end
+  end
+  
+  def void_last_transaction
+    @total -= @transactions.pop
+  end
+end
