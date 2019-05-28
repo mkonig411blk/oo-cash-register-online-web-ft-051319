b91d69beeb5ba6c69566222bf2744a02def0c5ce
Margot Konig <margot.konig@blackrock.com>
2019-05-28 17:00:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   
   def apply_discount
     # discount comes in as 20(%) instead of .20, so we divide by 100.0 so it is float and not an integer -- integers cannot have decimal values
-    @total = @total - (@total*(self.discount/100.0))
+    @total = @total - (@total*(discount/100.0))
    if discount > 0 
      "After the discount, the total comes to $#{@total.to_i}."
    else
