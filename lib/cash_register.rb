5e6647c69579510dc9931cb9ce192cd8702c4a32
Margot Konig <margot.konig@blackrock.com>
2019-05-28 17:07:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     quantity.times do 
     @total += price
   end
-  
+  @items << title
   end
   
   def apply_discount
@@ -25,7 +25,6 @@ class CashRegister
   end
   
   def items
-    
     @items
   end
 end
