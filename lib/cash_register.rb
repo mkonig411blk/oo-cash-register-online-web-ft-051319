22d793e0ad0c68164050fe0632bd6f09bbe9b772
Margot Konig <margot.konig@blackrock.com>
2019-05-28 16:56:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,6 +12,7 @@ class CashRegister
   end
   
   def apply_discount
+    # discount comes in as 20 instead of .20, so we divide by 100.0
     @total = @total - (@total*(self.discount/100.0))
   end
 end
