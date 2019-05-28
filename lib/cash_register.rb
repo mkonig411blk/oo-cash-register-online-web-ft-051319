2bfecdb08625f9c422ec4c988ede9b46cdc46fad
Margot Konig <margot.konig@blackrock.com>
2019-05-28 16:57:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,5 +14,6 @@ class CashRegister
   def apply_discount
     # discount comes in as 20(%) instead of .20, so we divide by 100.0 so it is float and not an integer -- integers cannot have decimal values
     @total = @total - (@total*(self.discount/100.0))
+    
   end
 end
